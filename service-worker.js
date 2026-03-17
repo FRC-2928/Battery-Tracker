7c647464260610ac7147163e495d88564ff209db
Turphin <45678161+Turphin@users.noreply.github.com>
2026-03-16 22:44:30 -0700

    fix offline service worker

--- service-worker.js
+++ service-worker.js
@@ -91,3 +91,20 @@
         )
     }
     })
+
+    const CACHE_NAME = 'cool-cache';
+
+    // Add whichever assets you want to pre-cache here:
+    const PRECACHE_ASSETS = [
+        '/Battery-Tracker/',
+        '/Battery-Tracker/index.html',
+        '/Battery-Tracker/icon.svg',
+    ]
+
+    // Listener for the install event - pre-caches our assets list on service worker install.
+    self.addEventListener('install', event => {
+        event.waitUntil((async () => {
+            const cache = await caches.open(CACHE_NAME);
+            cache.addAll(PRECACHE_ASSETS);
+        })());
+    });
